3a151be2c58d23e0583689748d9b693d4df461d2
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-03-28 09:51:36 +0200

    a

--- main.c
+++ main.c
@@ -28,10 +28,26 @@ int main(){
     // 65, 98
     // 010000001
     //char x = 'A'; // 65
+    unsigned int talet;
+    while(true){
+        printf("1. Mata in ett tal\n");        
+        printf("2. Skriv ut bittarna\n");        
+        printf("3. Set a bit\n");        
+        printf("4. Clear a bit\n");        
+        printf("Action:");
+        int action;
+        scanf(" %d",&action);
+        if(action == 1){
+            printf("Ange ett tal:");
+            scanf(" %d", &talet);
+        }
+
+    }
+
     unsigned int v = 0b10010000;
     printf("%d\n",v);
     printBits(sizeof(v),&v);
-    
+
     //unsigned int v = 12;
     return 0;
 }
